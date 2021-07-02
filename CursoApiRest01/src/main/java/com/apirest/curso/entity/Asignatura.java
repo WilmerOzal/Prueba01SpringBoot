af981cac5cf65ecff311c14298923aca2bb7994f
Alegria <Alegria@200.44.92.50>
2021-07-02 17:13:24 -0400

    Commit 03 para asignatura

--- CursoApiRest01/src/main/java/com/apirest/curso/entity/Asignatura.java
+++ CursoApiRest01/src/main/java/com/apirest/curso/entity/Asignatura.java
@@ -12,7 +12,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-//commit 02 para asignatura
+//commit 03 para asignatura
 //Asignaturas es como Materias
 @Entity
 @Table(name="asig_asignaturas", schema="public")
